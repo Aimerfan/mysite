733f47329d14ea74dd2211ee399eac2d59fa1cdf
aimerfan <jvuchein@gmail.com>
2020-06-30 11:37:59 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# mysite
+
+## Requirement package
+1. `django`
+
+## Useful command
+1. `python manage.py runserver` to run test server at localhost.
+2. `python manage.py shell` to start python Interactive interface with django parameter.
---
+++ polls/tests_views.py
@@ -0,0 +1,35 @@
+# python manage.py shell
+"""
+from django.test.utils import setup_test_environment
+setup_test_environment()
+"""
+
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class ViewsTests(TestCase):
+
+    # create an instance of the client for our use
+    client = Client()
+
+    def test_index_response_status_code(self):
+        response = ViewsTests.client.get('/')
+        # Not Found: /
+        # we should expect a 404 from that address; if you instead see an
+        # "Invalid HTTP_HOST header" error and a 400 response, you probably
+        # omitted the setup_test_environment() call described earlier.
+        self.assertEqual(response.status_code, 404)
+
+    def test_polls_response_status_code(self):
+        # on the other hand we should expect to find something at '/polls/'
+        # we'll use 'reverse()' rather than a hardcoded URL
+        response = ViewsTests.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
+        # 200
+
+# response.content
+# b'\n    <ul>\n    \n        <li><a href="/polls/1/">What&#x27;s up?</a></li>\n    \n    </ul>\n\n'
+
+# response.context['latest_question_list']
+# <QuerySet [<Question: What's up?>]>
